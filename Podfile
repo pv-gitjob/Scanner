9bba2786543e570bf36c6eeabdb462f18fc5d696
Praveen Vandeyar <praveenvandeyar@gmail.com>
2020-03-27 16:58:42 -0700

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Scanner' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Scanner
+  pod 'Alamofire', '~> 4.8.2'
+
+  target 'ScannerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ScannerUITests' do
+    # Pods for testing
+  end
+
+end
--- Scanner/ViewController.swift
+++ Scanner/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var textField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func clearBtn(_ sender: Any) {
+        self.textField.text.removeAll()
+    }
+    
+    @IBAction func cameraBtn(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        }else{
+            picker.sourceType = .photoLibrary
+        }
+        present(picker,animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as! UIImage
+        recognizeText(image: image.pngData())
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func recognizeText(image: Data?) {
+        let url = "http://ec2-54-241-187-187.us-west-1.compute.amazonaws.com/scanner/scan.php"
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                multipartFormData.append(image!, withName: "fileToUpload", fileName: "poop.png", mimeType: "image/png")
+        },
+            to: url,
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .success(let upload, _, _):
+                    upload.responseJSON { response in
+                        if let jsonResponse = response.result.value as? [String: Any] {
+                            self.textField.text = jsonResponse["text"] as? String
+                        }
+                    }
+                case .failure(let encodingError):
+                    print(encodingError)
+                }
+            }
+        )
+    }
+    
+    @IBAction func keyboardBtn(_ sender: Any) {
+        //self.textField.becomeFirstResponder()
+    }
+    
+    @IBAction func copyBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func saveBtn(_ sender: Any) {
+        
+    }
 
 }
 
